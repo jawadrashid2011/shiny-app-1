eb8f5069f7ebd9155f120435a014e06938f60512
Jawad Rashid <jawadrashid2010@gmail.com>
2014-05-17 11:21:49 +0500

    Formatted Text

--- ui.R
+++ ui.R
@@ -1,19 +1,23 @@
 library(shiny)
 
-# Define UI for application that draws a histogram
+# ui.R
+
 shinyUI(fluidPage(
         titlePanel("My Shiny App"),
-        
         sidebarLayout(
                 sidebarPanel(),
                 mainPanel(
-                        h1("First Level title"),
-                        h2("Second Level title"),
-                        h3("Third Level title"),
-                        h4("Fourth Level title"),
-                        h5("Fifth Level title"),
-                        h6("Sixth Level title")
+                        p("p creates a paragraph of text. Note: this paragraph is followed by br(), which makes a blank line."),
+                        p("A new p() command starts a new paragraph. Supply a style attribute to change the format of the entire paragraph", style = "font-family: 'times'; font-si16pt"),
+                        strong("strong() makes bold text."),
+                        em("em() creates italicized (i.e, emphasized) text."),
+                        br(),
+                        code("code displays your text similar to computer code"),
+                        div("div creates segments of text with a similar style. This division of text is all blue because I passed the argument 'style = color:blue' to div", style = "color:blue"),
+                        br(),
+                        p("span does the same thing as div, but it works with",
+                          span("groups of words", style = "color:blue"),
+                          "that appear inside a paragraph.")
                 )
         )
-      
 ))
